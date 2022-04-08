d71844a5a0b78123ad8180bd25697c4ecda3c6db
Curt-Lucas <60093618+Curt-Lucas@users.noreply.github.com>
2022-04-08 19:27:41 -0400

    init

---
+++ scrape.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const url = "https://www.rxlist.com/drug-slideshows-all/article.htm";
+
+async function scrapeData() {
+  try {
+    
+    const { data } = await axios.get(url);
+
+    const $ = cheerio.load(data);
+
+    const listItems = $("a");
+
+    const pageLinks = [];
+
+    listItems.each((idx, el) => {
+
+      const pageLink = { uri: "" };
+
+      pageLink.uri = $(el).attributes("href");
+
+      pageLinks.push(pageLink);
+    });
+
+    console.dir(pageLinks);
+
+    fs.writeFile("pageLinks.json", JSON.stringify(pageLinks, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Successfully written data to file");
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+// Invoke the above function
+scrapeData();
